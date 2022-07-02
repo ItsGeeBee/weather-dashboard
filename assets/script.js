4db351c4af7f87fb6d47fe5331ac53457da57906
ItsGeeBee <gracelbrown@hotmail.com>
2022-07-02 20:21:52 +1000

    Mixed up some jquery and js. Cleaned up. Search city can now append to UL list, no links

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,35 @@
 const apiKey = "bca39c92efc2381243d7834988122221";
 var searchbtn = document.querySelector('#searchBtn');
 var citySearch = document.querySelector('#citySearch');
-var searchHistory = document.querySelector('#searchHistory');
 var previousCities = [];
 const presentDay = moment().format('LL');
 
 
 // function for current weather condition 
 function currentWeather(city) {
-    var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  var weatherURL = `https://api.openweathermap.org/data/3.0/onecall?q=${city}&units=metric&appid=${apiKey}`; //template literal to interpolate varibale 
     // 'fetch' data from URL
     fetch(weatherURL) .then(function (response) {
       if (response.ok) { // check is response is okay
         console.log(response);
-        response.json().then(function (storedSearch) { // json to retreieve object 
-          console.log(storedSearch);
+      response.json().then(function (storedSearch) { // json to retreieve object 
+        console.log(storedSearch);
 
-          $("#weatherContent").css("display", "block");
-          $("#cityContent").empty();
+          // $("#weatherContent").css("display", "block");
+          // $("#cityContent").empty();
        
        
        let icon = storedSearch.weather[0].icon;
        let iconURL = `https://openweathermap.org/img/w/${icon}.png`;
 
        let cityCurrent =  
-       $(`<h2 id="cityCurrent"> ${storedSearch.name} ${presentDay} <img src="${iconURL}"/></h2>
+       $(`<h3 id="cityCurrent"> ${storedSearch.name} ${presentDay} <img src="${iconURL}"/></h3>
        <p>Temperature: ${storedSearch.main.temp} °C</p>
        <p>Wind Speed: ${storedSearch.wind.speed} KPH</p>
-       <p>Humidity: ${storedSearch.main.humidity}\%</p>`);
+       <p>Humidity: ${storedSearch.main.humidity}\%</p>
+       <p>UV Index: ${storedSearch.wind.speed} UV</p>`);
 
-       $("#weatherContent").append(cityCurrent); // apend to HTMl - BCS help 
+       $("#weatherContent").append(cityCurrent); // append to HTMl - BCS help 
         });
        } })
   } 
@@ -41,18 +41,20 @@ function currentWeather(city) {
 
 function inputCity(event){
     event.preventDefault(); // prevent page refresh //
-
-    var city = citySearch.value.trim(); // extract input data //
+    var city = citySearch.value.trim(""); // extract input data 
     
-    currentWeather(city); // excicute currentWeather function 
-
-if(previousCities.includes(city)) {
+    if(previousCities.includes(city)) {
+      return;
+    } else { 
     previousCities.push(city); 
 
-var history = $(`<li class="list-group-item">${city}</li>`); // template literal to interpolate varibale 
-    searchHistory.append(history);
+    let history = $(`<li">${city}</li>`);
+    
+    $("#searchHistory").append(history);
 }
 localStorage.setItem("city", JSON.stringify(previousCities)); // stringify to store in local storage 
+
+currentWeather(city); // excicute currentWeather function 
 }
 
 
