e838034b1b568b6280476ba1f1e8f666296d58ac
ItsGeeBee <gracelbrown@hotmail.com>
2022-07-02 13:35:55 +1000

    fixed API key issue. Added HTML space for generated content. Currently issue with addclas()

--- assets/script.js
+++ assets/script.js
@@ -5,47 +5,44 @@ var searchHistory = document.querySelector('#searchHistory');
 var previousCities = [];
 
 
+// function for current weather condition 
 function currentWeather(city) {
-    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}";
-    
-    fetch(weatherURL) // 'fetch' data from URL
-    .then(function (response) {
+    var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    // 'fetch' data from URL
+    fetch(weatherURL) .then(function (response) {
       if (response.ok) { // check is response is okay
         console.log(response);
         response.json().then(function (data) { // json to retreieve object 
           console.log(data);
-          displayRepos(data, user);
+
+          $("#showCurrent").addclass("displayBlock");
+          $("#cityContent").empty();
         });
-      } else {
-        alert('Error: ' + response.statusText); // if no valid repos - error messag e
-      }
-    })
+       } })
+  } 
 
-
-}
 
 
 
 // Submit function for user inout city //
 
 function inputCity(event){
-    event.preventDefault(); // present page refresh //
+    event.preventDefault(); // prevent page refresh //
 
     var city = citySearch.value.trim(); // extract input data //
-    currentWeather(city);
+    currentWeather(city); // excicute currentWeather function 
 
 if(previousCities.includes(city)) {
     previousCities.push(city); 
 
-
-var history = $(`<li class="list-group-item">${city}</li>`);
+var history = $(`<li class="list-group-item">${city}</li>`); // template literal to interpolate varibale 
     searchHistory.append(history);
 }
-localStorage.setItem("city", JSON.stringify(previousCities));
+localStorage.setItem("city", JSON.stringify(previousCities)); // stringify to store in local storage 
 }
 
 
 // event listener //
-searchbtn.addEventListener('submit', inputCity);
+searchbtn.addEventListener('click', inputCity);
 
 
