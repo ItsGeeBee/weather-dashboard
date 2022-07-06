825881e0a95a5497ed4d656e7a1b060350f7b8bb
ItsGeeBee <gracelbrown@hotmail.com>
2022-07-06 18:32:23 +1000

    worked on forecast function with TA. Correct response from API.

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,73 @@
 const apiKey = "bca39c92efc2381243d7834988122221";
-var searchbtn = document.querySelector('#searchBtn');
-var citySearch = document.querySelector('#citySearch');
-var previousCities = [];
+let searchbtn = document.querySelector('#searchBtn');
+let citySearch = document.querySelector('#citySearch');
+let previousCities = [];
 const presentDay = moment().format('LL');
 
 
 // function for current weather condition 
 function currentWeather(city) {
-  var weatherURL = `https://api.openweathermap.org/data/3.0/onecall?q=${city}&units=metric&appid=${apiKey}`; //template literal to interpolate varibale 
-    // 'fetch' data from URL
-    fetch(weatherURL) .then(function (response) {
+  let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`; // interpolate variables
+  // 'fetch' data from URL
+  fetch(weatherURL) .then(function (response) {
       if (response.ok) { // check is response is okay
-        console.log(response);
+      console.log(response);
       response.json().then(function (storedSearch) { // json to retreieve object 
-        console.log(storedSearch);
-
-          // $("#weatherContent").css("display", "block");
-          // $("#cityContent").empty();
+      console.log(storedSearch);
        
        
-       let icon = storedSearch.weather[0].icon;
-       let iconURL = `https://openweathermap.org/img/w/${icon}.png`;
+  let icon = storedSearch.weather[0].icon;
+  let iconURL = `https://openweathermap.org/img/w/${icon}.png`;
 
-       let cityCurrent =  
-       $(`<h3 id="cityCurrent"> ${storedSearch.name} ${presentDay} <img src="${iconURL}"/></h3>
+  let cityCurrent =  
+       $(`<h4 id="cityCurrent"> ${storedSearch.name} ${presentDay} <img src="${iconURL}"/></h4>
        <p>Temperature: ${storedSearch.main.temp} °C</p>
        <p>Wind Speed: ${storedSearch.wind.speed} KPH</p>
-       <p>Humidity: ${storedSearch.main.humidity}\%</p>
-       <p>UV Index: ${storedSearch.wind.speed} UV</p>`);
+       <p>Humidity: ${storedSearch.main.humidity}\%</p>`);
 
        $("#weatherContent").append(cityCurrent); // append to HTMl - BCS help 
-        });
-       } })
-  } 
+       let lat = storedSearch.coord.lat;
+       let lon = storedSearch.coord.lon;
+       console.log(lat,lon);
 
+    forecast(city);
+    });
+  }})
+};
+// function for 5 day forecast 
+     function  forecast (city) {
 
+  let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;  
+  console.log(forecastURL);
+  // 'fetch' data from URL
+  fetch(forecastURL) .then(function (forecastResponse) {
+    if (forecastResponse.ok) { // check is response is okay
+      console.log(forecastResponse);
+      response.json().then(function (array) {
 
+        let fiveDayArray = array.list.filter(day => day.dt_txt.includes('15:00:00')); // specify what time of day https://stackoverflow.com/questions/53431440/filter-api-array
+
+ }})
+};
 
 // Submit function for user inout city //
 
 function inputCity(event){
-    event.preventDefault(); // prevent page refresh //
-    var city = citySearch.value.trim(""); // extract input data 
-    
-    if(previousCities.includes(city)) {
-      return;
-    } else { 
-    previousCities.push(city); 
+   event.preventDefault(); // prevent page refresh //
+
+    let city = citySearch.value.trim(""); // extract input data 
+  
+    previousCities.push(city); // push to array 
+
+    localStorage.setItem("city", JSON.stringify(previousCities));// stringify to store in local storage 
 
     let history = $(`<li">${city}</li>`);
-    
     $("#searchHistory").append(history);
-}
-localStorage.setItem("city", JSON.stringify(previousCities)); // stringify to store in local storage 
 
-currentWeather(city); // excicute currentWeather function 
-}
 
+    currentWeather(city); // run currentWeather function 
+   
+};
 
 // event listener //
 searchbtn.addEventListener('click', inputCity);
